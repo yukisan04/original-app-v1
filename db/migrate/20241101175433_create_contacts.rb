3153059baa3aaacfd9fe79821f3112fd70be1c83
yukisan04 <yuki28yukinko2525@icloud.com>
2024-11-02 16:32:04 +0900

    電話番号を削除

--- db/migrate/20241101175433_create_contacts.rb
+++ db/migrate/20241101175433_create_contacts.rb
@@ -3,7 +3,6 @@ class CreateContacts < ActiveRecord::Migration[7.0]
     create_table :contacts do |t|
       t.string :email
       t.text :content
-      t.string :phone_number
 
       t.timestamps
     end
