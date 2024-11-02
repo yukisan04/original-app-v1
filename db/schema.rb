cde770f8c37f78fed651f9686d0b102db4e6d149
yukisan04 <yuki28yukinko2525@icloud.com>
2024-11-02 15:34:37 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_11_02_042448) do
-  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_02_042448) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb3", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,13 +33,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_02_042448) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "contacts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "contacts", charset: "utf8mb3", force: :cascade do |t|
     t.string "email"
     t.text "content"
     t.string "phone_number"
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_02_042448) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
     t.string "urgency"
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_02_042448) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "memberships", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "memberships", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "room_id", null: false
     t.datetime "created_at", null: false
@@ -66,8 +66,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_02_042448) do
     t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
-  create_table "rooms", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "rooms", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
@@ -75,25 +76,26 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_02_042448) do
     t.index ["name"], name: "index_rooms_on_name", unique: true
   end
 
-  create_table "rooms_users", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "rooms_users", id: false, charset: "utf8mb3", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "room_id", null: false
+    t.index ["room_id"], name: "index_rooms_users_on_room_id"
+    t.index ["user_id"], name: "index_rooms_users_on_user_id"
   end
 
-  create_table "stocks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "stocks", charset: "utf8mb3", force: :cascade do |t|
     t.string "product_name", null: false
     t.integer "quantity", default: 0
     t.string "image"
     t.bigint "room_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "stock_status"
     t.string "image_url"
     t.string "external_image_url"
     t.index ["room_id"], name: "index_stocks_on_room_id"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -109,6 +111,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_02_042448) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "memberships", "rooms"
-  add_foreign_key "memberships", "users"
+  add_foreign_key "memberships", "users", on_delete: :cascade
   add_foreign_key "stocks", "rooms"
 end
