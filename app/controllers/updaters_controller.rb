d24a917e52c1505b915d12d88fd2e2f9f716d8a8
yukisan04 <yuki28yukinko2525@icloud.com>
2024-11-06 17:13:57 +0900

    更新情報ページ更新

--- app/controllers/updaters_controller.rb
+++ app/controllers/updaters_controller.rb
@@ -1,5 +1,34 @@
 class UpdatersController < ApplicationController
+  before_action :authenticate_admin!, only: [:new]  # 管理者のみアクセス可能
+
+  def index
+    @updaters = Updater.all.order(created_at: :desc)
+  end
+
   def new
-    @updaters = Updater.all
+    @updater = Updater.new
+  end
+
+  def create
+    @updater = Updater.new(updater_params)
+    if @updater.save
+      redirect_to updaters_path, notice: '更新情報が追加されました'
+    else
+      render :new
+    end
+  end
+
+  def admin?
+    self.admin
+  end
+
+  private
+
+  def updater_params
+    params.require(:updater).permit(:title, :content)
+  end
+
+  def authenticate_admin!
+    redirect_to root_path, alert: '管理者権限が必要です' unless current_user&.admin?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_06_053723) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_06_080927) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -111,6 +111,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_06_053723) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
