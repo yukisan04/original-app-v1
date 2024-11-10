f31d94d00e76ac9b224c27bd3430a62d57dff5d4
yukisan04 <yuki28yukinko2525@icloud.com>
2024-11-10 20:03:15 +0900

    t

--- db/migrate/20241110105933_add_status_to_contacts.rb
+++ db/migrate/20241110105933_add_status_to_contacts.rb
@@ -1,5 +1,5 @@
 class AddStatusToContacts < ActiveRecord::Migration[7.0]
   def change
-    add_column :contacts, :status, :integer, default: 0, null: false
+    change_column :contacts, :status, :integer, default: 0, null: false
   end
 end
