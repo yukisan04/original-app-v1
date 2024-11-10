66521ce5c9b28b2d42ef707d223e95620fee3810
yukisan04 <yuki28yukinko2525@icloud.com>
2024-11-11 00:27:30 +0900

    t

--- db/migrate/20241110151305_add_user_id_and_edited_to_replies.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdAndEditedToReplies < ActiveRecord::Migration[7.0]
-  def change
-    add_column :replies, :user_id, :integer
-    add_column :replies, :edited, :boolean, default: false
-  end
-end
